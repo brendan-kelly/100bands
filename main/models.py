3cc1846ffdbcff50eff054315b2c0cca10a59441
Austin Buckler <buckleraustin@gmail.com>
2017-08-27 00:55:40 -0700

    added user profile model, with hooks

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 WANT_LEVEL = ((5, 'DEFINITELY'),
@@ -7,6 +9,23 @@ WANT_LEVEL = ((5, 'DEFINITELY'),
     (3, 'MAYBE'),
     (2, 'NOT SURE'),
     (1, 'DONT CARE'))
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # TODO: generate unique hash or random names for share_link.
+    share_link = models.CharField(max_length=256, blank=False, editable=False)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Concert(models.Model):
