f7dca8347fedac5bd5e195ab13084797bac8e389
Austin Buckler <buckleraustin@gmail.com>
2017-08-27 00:29:49 -0700

    fixed migration

--- main/models.py
+++ main/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Concert(models.Model):
-    band = models.CharField(required=True, max_length=128, blank=False, default='')
-    date = models.DateField(required=True)
-    city = models.CharField(required=True, max_length=128, blank=False, default='')
-    venue = models.CharField(required=False, max_length=128, blank=False, default='')
+    band = models.CharField(max_length=128, blank=False, default='')
+    date = models.DateField(blank=False)
+    city = models.CharField(max_length=128, blank=False, default='')
+    venue = models.CharField(max_length=128, blank=True, default='')
     want_level = models.IntegerField(max_length=5)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
